14a35dd673b46ba9860516a692edc5e6d14cfee5
Daniel Glaser <me@the78mole.de>
2026-04-27 14:09:09 +0200

    fix: update favicon link to use SVG format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
         { property: 'og:site_name', content: 'BizzMark' },
       ],
       link: [
-        { rel: 'icon', href: '/favicon.ico' },
+          { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@600;700&display=swap',
